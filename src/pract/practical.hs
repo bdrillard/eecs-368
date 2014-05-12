976f33dd18e0f744d0eb2239c433ef77d8b6f1a7
Aleksander Eskilson <aleksanderesk@gmail.com>
2014-05-12 15:00:56 -0500

    updated

--- src/pract/practical.hs
+++ src/pract/practical.hs
@@ -14,21 +14,14 @@ main = blankCanvas 3000 $ go
 go :: Context -> IO()
 go context = do
     (width,height) <- send context size
-    let loop branches iterations cols = do
-        send context $ do
-            sequence_ [ showBranch branch | branch <- branches ]
-        threadDelay (20 * 1000)
-        
-        es <- flush context
-
-        loop (
-    loop [((width / 2, height - 50),(width / 2, height / 2 - 50),"red")] 0 (cycle ["red","blue","yellow"])
+    send context $ do
+        showBranch ((width / 2, height - 50),(width / 2, (height - 50) - height * 0.5),"red")
 
 showBranch :: Branch -> Canvas()
 showBranch ((x1,y1),(x2,y2),col) = do
     moveTo(x1,y1)
     lineTo(x2,y2)
-    lineWidth 10
+    lineWidth 5
     strokeStyle col
     stroke()
         
